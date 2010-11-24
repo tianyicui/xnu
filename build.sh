5603755ac4fd4d7c6c4b9fce417bf1465f66a227
Tianyi Cui <tianyicui@gmail.com>
2010-11-25 01:56:51 +0800

    right way to specify CFLAGS

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 set -e
 unset LS_COLORS
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin"
-export CFLAGS="-O3 -march=core2 -msse4.1 -w -pipe"
+
 rm -rf BUILD
 make \
 	MAKEJOBS=-j4 \
 	ARCH_CONFIGS="I386 X86_64" \
 	KERNEL_CONFIGS="RELEASE" \
 	VERBOSE=YES \
+	RC_CFLAGS="-O3 -march=core2 -msse4.1 -w -pipe" \
 	SDKROOT=/Developer/SDKs/MacOSX10.6.sdk \
 	install
